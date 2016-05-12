49522aebd423f47d90cef6973c23ef652f9b3a0d
Jason McVeigh <jmcveigh@runt.office.local>
2016-05-11 21:46:04 -0400

    p5-graphics-vtk: distance_between_points.p.

---
+++ demos/distance_between_points.pl
@@ -0,0 +1,24 @@
+#!/bin/perl
+
+use strict;
+use warnings;
+
+use feature 'say';
+
+use Graphics::VTK qw(:python);
+
+my ($p0,$p1) = (
+	[0,0,0],
+	[1,1,1]
+);
+
+my $math = vtk::vtkMath();
+
+my $dist_squared = $math->Distance2BetweenPoints($p0,$p1);
+ 
+my $dist = sqrt($dist_squared);
+ 
+say("p0 = ", @{$p0});
+say("p1 = ", @{$p1});
+say("distance squared = ", $dist_squared);
+say("distance = ", $dist);
